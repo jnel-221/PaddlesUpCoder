3304dda61c9e6d11929b9a26d87485d05e70ac13
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-03-19 20:01:18 -0500

    created seed data and seed.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');//see line 11 to see where requirement used
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 
