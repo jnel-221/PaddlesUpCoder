7fa5c2744f0c117de337eaf7527557a26d4f4a01
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-04-05 15:30:28 -0500

    created updatepost handlebars and new partial for dashboard posts

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -3,27 +3,30 @@ const { User, Post } = require("../../models");
 // const withAuth = require("../../utils/auth");
 
 router.post("/newpost", async (req, res) => {
-
   try {
-     await Post.create({
+    await Post.create({
       title: req.body.blogTitle,
       text: req.body.blogContent,
       user_id: req.session.user_id,
     });
 
-    const postData = await User.findByPk(req.session.user_id,
-      {
-        attributes: {
-          exclude: ["password"]},
-          include: [{model:Post}],
-        });
-     
-    const posts = postData.get({plain:true});
+    const postData = await User.findByPk(req.session.user_id, {
+      attributes: {
+        exclude: ["password"],
+      },
+      include: [{ model: Post }],
+    });
 
-    const dashboardPosts = posts.posts
-  
+    const posts = postData.get({ plain: true });
 
+    console.log("@@Here's the plain data", posts);
+
+    const user = posts.user_name;
+    const dashboardPosts = posts.posts;
+
+    console.log("@@Here's the user variable; ", user);
     res.render("dashboard", {
+      user,
       dashboardPosts,
       logged_in: req.session.logged_in,
     });
