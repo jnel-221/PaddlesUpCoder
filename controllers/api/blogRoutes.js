adb50f10da56e4ae5477fed1a65079046fd240f6
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-04-06 15:27:20 -0500

    fixed bug on script tags in handlebars, delete post now functions

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { User, Post } = require("../../models");
-// const withAuth = require("../../utils/auth");
+const withAuth = require("../../utils/auth");
 
 //create new blog post
 router.post("/newpost", async (req, res) => {
@@ -46,9 +46,31 @@ router.post("/:id", async (req, res) => {
       post,
       logged_in: req.session.logged_in,
     });
-    
+
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.render("dashboard", {
+    logged_in: req.session.logged_in, 
+    });
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
--- public/js/updateblogpost.js
+++ public/js/updateblogpost.js
@@ -28,3 +28,25 @@
 
 // // //   document.querySelectorAll(".card-header").forEach(function(element) 
 // //   {element.addEventListener("click", grabIdHandler)});
+
+//delete button handler
+const delButtonHandler = async (event) => {
+    // event.preventDefault;
+    if (event.target.hasAttribute('data-delete')) {
+      const id = event.target.getAttribute('data-delete');
+        console.log("updateblogpost.js del handler", id);
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete project');
+      }
+    }
+  };
+
+  document
+  .querySelector('#btn-delete')
+  .addEventListener('click', delButtonHandler);
