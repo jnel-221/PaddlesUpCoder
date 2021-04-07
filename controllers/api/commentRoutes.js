84dd6beaaf82fb9a19a60d4c50f20e407585e9b9
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-04-07 08:57:28 -0500

    route for comments successfully receiving posts; viewonepost view created

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const { Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+router.post("/", async (req, res) => {
+  try {
+    
+    const response = await Comment.create({
+      text: req.body.blogContent,
+      user_id: req.body.user_id,
+      post_id: req.body.post_id,
+    });
+    
+    
+    res.status(200).json({ msg: "Comment posted", response });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
